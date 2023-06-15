26fe269667eec33a43103f0e05f8193ec1695b58
Vasiliy Shchelokov <vshchelokov1@yandex.ru>
2023-06-15 23:31:56 +0300

    Правки ревью. Итерация 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,28 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button')
 let profileName = document.querySelector('.profile__name');
-let popupName = document.querySelector('.popup__name');
+let popupName = document.querySelector('#name');
 let profileOccupation = document.querySelector('.profile__occupation');
-let popupOccupation = document.querySelector ('.popup__occupation');
+let popupOccupation = document.querySelector ('#occupation');
 let formElement = document.querySelector('.popup__form');
-
-popupName.value = profileName.textContent;
-popupOccupation.value = profileOccupation.textContent;
 
 function editButtonClick() {
   popup.classList.toggle('popup_opened');
+  popupName.value = profileName.textContent;
+  popupOccupation.value = profileOccupation.textContent;
 }
 
 function closePopupButtonClick() {
   popup.classList.toggle('popup_opened');
 }
 
-editButton.addEventListener('click', editButtonClick);
-closePopupButton.addEventListener('click', closePopupButtonClick);
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileOccupation.textContent = popupOccupation.value;
-    popup.classList.toggle('popup_opened');
+    closePopupButtonClick();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
+editButton.addEventListener('click', editButtonClick);
+closePopupButton.addEventListener('click', closePopupButtonClick);
