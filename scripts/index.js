15a3172079733532779b977b8ae09a69ca3368fb
Vasiliy Shchelokov <vshchelokov1@yandex.ru>
2023-06-28 00:51:47 +0300

    добавил кнопку удаления, поп-ап добавления места, доработал логику работы с поп-апами

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,10 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-button')
+const addButton = document.querySelector('.profile__add-button');
+const popupEdit = document.getElementById('popup-edit');
+const popupAdd = document.getElementById('popup-add');
+
+const closeButtonEditPopup = document.getElementById('popup-edit-close-button')
+const closeButtonAddPopup = document.getElementById('popup-add-close-button')
 let profileName = document.querySelector('.profile__name');
 let popupName = document.querySelector('#name');
 let profileOccupation = document.querySelector('.profile__occupation');
@@ -8,22 +12,32 @@ let popupOccupation = document.querySelector ('#occupation');
 let formElement = document.querySelector('.popup__form');
 
 function editButtonClick() {
-  popup.classList.toggle('popup_opened');
+  popupEdit.classList.toggle('popup_opened');
   popupName.value = profileName.textContent;
   popupOccupation.value = profileOccupation.textContent;
 }
 
-function closePopupButtonClick() {
-  popup.classList.toggle('popup_opened');
+function addButtonClick() {
+  popupAdd.classList.toggle('popup_opened');
+}
+
+function closeEditPopupButtonClick() {
+  popupEdit.classList.toggle('popup_opened');
+}
+
+function closeAddPopupButtonClick() {
+  popupAdd.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileOccupation.textContent = popupOccupation.value;
-    closePopupButtonClick();
+    closeEditPopupButtonClick();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
 editButton.addEventListener('click', editButtonClick);
-closePopupButton.addEventListener('click', closePopupButtonClick);
+addButton.addEventListener('click', addButtonClick)
+closeButtonEditPopup.addEventListener('click', closeEditPopupButtonClick);
+closeButtonAddPopup.addEventListener('click', closeAddPopupButtonClick);
