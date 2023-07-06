f60ebb024a06e55b09ddc4a59cbbab2c2a6a8b43
Vasiliy Shchelokov <vshchelokov1@yandex.ru>
2023-07-06 23:05:15 +0300

    Добавлен рендеринг карточек, лайк, удаление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,25 @@ const popupAdd = document.getElementById('popup-add');
 
 const closeButtonEditPopup = document.getElementById('popup-edit-close-button')
 const closeButtonAddPopup = document.getElementById('popup-add-close-button')
-let profileName = document.querySelector('.profile__name');
-let popupName = document.querySelector('#name');
-let profileOccupation = document.querySelector('.profile__occupation');
-let popupOccupation = document.querySelector ('#occupation');
-let formElement = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const popupName = document.querySelector('#name');
+const profileOccupation = document.querySelector('.profile__occupation');
+const popupOccupation = document.querySelector ('#occupation');
+const formEditElement = document.querySelector('.popup__form');
+const locationBlock = document.querySelector('.location');
+const templateElement = document.querySelector('.location-template');
+const formAddElement = document.getElementById('formAdd');
+const inputNewNameElement = document.getElementById('newName');
+const inputNewLinkElement = document.getElementById('newImageLink');
+
+const initialCards = [
+  { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'},
+  { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'},
+  { name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'},
+  { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
+  { name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'},
+  { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
+];
 
 function editButtonClick() {
   popupEdit.classList.toggle('popup_opened');
@@ -29,15 +43,57 @@ function closeAddPopupButtonClick() {
   popupAdd.classList.toggle('popup_opened');
 }
 
-function handleFormSubmit (evt) {
+function handleEditFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileOccupation.textContent = popupOccupation.value;
+
     closeEditPopupButtonClick();
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+const createCard = ({ name, link }) => {
+  const clone = templateElement.content.cloneNode(true);
+  const cardElement = clone.querySelector('.location__item');
+  const imageElement = cardElement.querySelector('.location__image');
+  imageElement.setAttribute('src', link);
+  imageElement.setAttribute('alt', name);
+  cardElement.querySelector('.location__name').textContent = name;
+
+  const deleteButton = cardElement.querySelector(".location__trash");
+
+  deleteButton.addEventListener('click', () => {
+    cardElement.remove();
+  });
+
+  const likeButton = cardElement.querySelector('.location__like');
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('location__like-active');
+  });
+
+  return cardElement;
+}
+
+ initialCards.forEach((item) => {
+  const cardElement = createCard(item);
+  locationBlock.append(cardElement);
+})
+
+const handleAddFormSubmit = (e) => {
+  e.preventDefault();
+  let name = inputNewNameElement.value;
+  let link = inputNewLinkElement.value;
+  const cardElement = createCard({ name, link });
+  initialCards.unshift(cardElement);
+  locationBlock.prepend(cardElement);
+  inputNewNameElement.value = '';
+  inputNewLinkElement.value = '';
+  closeAddPopupButtonClick();
+
+} 
+
+formEditElement.addEventListener('submit', handleEditFormSubmit); 
 editButton.addEventListener('click', editButtonClick);
-addButton.addEventListener('click', addButtonClick)
+addButton.addEventListener('click', addButtonClick);
 closeButtonEditPopup.addEventListener('click', closeEditPopupButtonClick);
 closeButtonAddPopup.addEventListener('click', closeAddPopupButtonClick);
+formAddElement.addEventListener('submit', handleAddFormSubmit);
